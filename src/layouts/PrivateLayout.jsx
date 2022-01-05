4a4b9458665efe95718b98812f508b99207da9dc
Yecasu <yecasu182@gmail.com>
2022-01-05 14:56:07 -0500

    agregar boton de login en navbar

--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -3,12 +3,12 @@ import Sidebar from "../components/Sidebar";
 
 
 const PrivateLayout = ({children}) => {
-    return (
-        <div className="flex-sidebar">
-            <Sidebar />
-            <main className="flex-main">{children}</main>
-        </div>
-    )
+  return (
+    <div className="flex w-screen h-screen">
+        <Sidebar />
+        <main className='flex w-full bg-blue-400 overflow-y-scroll'>{children}</main>   
+    </div>
+    );
 };
 
 export default PrivateLayout;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,39 +4,8 @@ import {Link} from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className="login-center">
-      <h2 className="titulo">Inicia sesión en tu cuenta</h2>
-      <form className="formulario">
-        <div>
-          <input className="campo" type="email" placeholder="Correo Electrónico" required />
-          <input className="campo-dos" type="password" placeholder="Contraseña"  required />
-        </div>
-        <div className="recu">
-          <div>
-            <label htmlFor="recuerdame">
-              <input type="checkbox" name="recuerdame" />
-              Recuerdame
-            </label>
-          </div>
-        <div>
-          <Link to="/">¿Olvidaste tu contraseña?</Link>
-        </div>
-        </div>
-        <div>
-          <Link to="/admin">
-            <button type="submit">Iniciar Sesión</button>
-          </Link>
-        </div>
-        <div>
-          O
-        </div>
-        <div>
-          < img src={Google} alt="Logo Google" className="google" />
-          <button>Continua con Google</button>
-        </div>
-      </form>
-    </div>
-  );
+    
+  )
 };
 
 export default Login;
