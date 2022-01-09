dfa518908069b72cb7f439a06300802f6feb219f
Yecasu <yecasu182@gmail.com>
2022-01-09 12:59:15 -0500

    agregar ejemplo de renderizacion condicional

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -1,51 +1,63 @@
 import React, { useEffect, useState } from 'react';
 
+//realizar un formulario que le pida al usuario su edad y muestre un mensaje
+// que diga si el usuario es mayor de edad o no
+
 const Vehiculos = () => {
-  const [nombreVehiculo , setNombreVehiculo] = useState("");
+  const [edad, setEdad] = useState(0);
+  const [esMenorDeEdad,setEsMenorDeEdad] = useState(false);
+  const [mostrarCamposAdicionales, setMostrarCamposAdicionales] = useState(false);
   
-  useEffect(() => {
-    console.log(
-      "Hola, soy un use effect que se ejecuta solo una vez cuando la pagina se renderiza porque tiene el array de dependencias vacío"
-    );
-    //paso 2
-    //paso 3
-    //paso 4
-  },[]);   //una función que se ejecuta al inicio de la renderización del código
-
-  useEffect(() => {
-    console.log("Esto es una funcion que se ejecuta cada que cambia el valor de nombrevehiculo");
-    console.log("el valor de la variable es ", nombreVehiculo);
-  }, [nombreVehiculo]); //una funcion que se ejecuta cada que una variable cambia
-
-  // useEffect(() => {
-  //   console.log("este es un use effect que se ejecuta SIEMPRE que cambia una variable");
-  //}); se explica solo para que sepan que existe, pero no se debe usar. 
-
- const enviarDatosAlBackend = ()=>{
-   console.log("El valor de la variable nombreVehiculo es ", nombreVehiculo);
- }
+  useEffect(()=> {
+    if (edad >= 18) {
+      setEsMenorDeEdad(false);
+    } else {
+      setEsMenorDeEdad(true);
+    }
+  }, [edad]);
 
   return (
     <form className='flex flex-col'>
       <h2>Formulario de Creación de vehículos</h2>
-      <input 
-        onChange={(e)=> {
-          setNombreVehiculo(e.target.value);
-        }} 
-        type="text"
-        placeholder="Nombre del vehículo"
-      />
-      <input 
-        onChange={(e)=> {
-          console.log(e.target.value);
-        }}      
-        type="text" 
-        placeholder="Marca del vehículo"
-      />
-      <input type="text" placeholder="Modelo del vehículo"/>
-      <button type="button" onClick={enviarDatosAlBackend} className='bg-indigo-500 text-white'>Enviar Datos</button>
+      <label htmlFor='edad'>
+        Por favor ingrese su edad
+        <input 
+          value={edad} 
+          onChange={(e)=>
+            {setEdad(e.target.value);
+          }}
+          className='border border-gray-600' 
+          name='edad' 
+          type="number" 
+        />
+      </label>
+      {esMenorDeEdad ? (
+        <span className='text-3xl text-red-500'>
+          ¡Usted es menor de edad, no puede hacer pagos!
+        </span> 
+        ) : (
+          <span className='text-3xl text-green-500'>
+            ¡Usted es mayor de edad, puede hacer pagos!
+          </span>
+        )}
+        <button 
+          onClick={() => setMostrarCamposAdicionales(!mostrarCamposAdicionales)} 
+          type="button" 
+          className='text-white bg-indigo-500'
+        >
+          Mostrar campos adicionales
+        </button>
+        {mostrarCamposAdicionales && (
+          <div>
+            <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type="text" />
+            <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type="text" />
+            <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type="text" />
+            <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type="text" />
+            <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type="text" />
+          </div>
+        )}
     </form>
   );
-};    //funciones que se ejecutan cada que yo hago cambios en un input o cada que le hago click a un buton
+};   
 
 export default Vehiculos;
