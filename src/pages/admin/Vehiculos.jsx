b55de9fc4ab0a517e7d33f8c6e2ca0501b73a79f
Yecasu <yecasu182@gmail.com>
2022-01-11 16:53:31 -0500

    agregar funcionalidad para que el formulario envíe datos a la lista de vehículos

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -1,63 +1,206 @@
 import React, { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 //realizar un formulario que le pida al usuario su edad y muestre un mensaje
 // que diga si el usuario es mayor de edad o no
 
+const vehiculosBackend = [
+  {
+    nombre:"Corolla",
+    marca:"Toyota",
+    modelo:2014
+  },
+  {
+    nombre:"Sandero",
+    marca:"Renault",
+    modelo:2020
+  },
+  {
+    nombre:"Rav4",
+    marca:"Toyota",
+    modelo:2021
+  },
+  {
+    nombre:"Fiesta",
+    marca:"Ford",
+    modelo:2017
+  },
+  {
+    nombre:"Mazda 3",
+    marca:"Mazda",
+    modelo:2020,
+  },
+  {
+    nombre:"Chevrolet",
+    marca:"Onix",
+    modelo:2020,
+  },
+]
+
 const Vehiculos = () => {
-  const [edad, setEdad] = useState(0);
-  const [esMenorDeEdad,setEsMenorDeEdad] = useState(false);
-  const [mostrarCamposAdicionales, setMostrarCamposAdicionales] = useState(false);
-  
-  useEffect(()=> {
-    if (edad >= 18) {
-      setEsMenorDeEdad(false);
-    } else {
-      setEsMenorDeEdad(true);
+  const [mostrarTabla, setMostrarTabla] = useState(true);
+  const [vehiculos, setVehiculos] = useState([]);
+  const [textoBoton,setTextoBoton] = useState("Crear Nuevo Vehículo");
+  const [colorBoton, setColorBoton] = useState("indigo");
+
+  useEffect(() => {
+    //obtener lista de vehiculos desde el backend
+    setVehiculos(vehiculosBackend);
+  }, []);
+
+  useEffect(()=>{
+    if(mostrarTabla){
+      setTextoBoton("Crear Nuevo Vehículo");
+      setColorBoton("indigo");
     }
-  }, [edad]);
+    else{
+      setTextoBoton("Mostrar todos los vehículos");
+      setColorBoton("red");
+    }
+  },[mostrarTabla])
+  return (
+    <div className='flex h-full w-full flex-col items-center justify-start p-8'>
+      <div className='flex flex-col'>
+        <h2 className='text-3xl font-extrabold text-gray-900'>
+          Página de administración de vehículo
+        </h2>
+        <button 
+          onClick={()=>{
+            setMostrarTabla(!mostrarTabla);
+          }} 
+          className={`text-white bg-${colorBoton}-500 p-5 rounded-full m-6 w-28 self-end`}
+        >
+          {textoBoton}
+        </button>
+      </div>
+      {mostrarTabla ? (
+        <TablaVehiculos listaVehiculos={vehiculos} /> 
+      )  : (
+        <FromularioCreacionVehiculos 
+          funcionParaMostrarLaTabla={setMostrarTabla} 
+          listaVehiculos={vehiculos}
+          funcionParaAgregarUnVehiculo={setVehiculos}
+        /> 
+      )}
+      <ToastContainer position="bottom-center" autoClose={5000} />
+    </div>
+  );
+};
+
+const TablaVehiculos = ({ listaVehiculos }) => {
+  useEffect(()=>{
+    console.log("este es el listado de vehiculos en el componente de tabla", listaVehiculos)
+  },[listaVehiculos]);
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <h2 className='text-2xl font-extrabold text-gray-800'>Todos los vehículos</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Nombre del vehículo</th>
+          <th>Marca del vehículo</th>
+          <th>Modelo del vehículo</th>
+        </tr>
+      </thead>
+      <tbody>
+        {listaVehiculos.map((vehiculo) => {
+          return (
+            <tr>
+              <td>{vehiculo.nombre}</td>
+              <td>{vehiculo.marca}</td>
+              <td>{vehiculo.modelo}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+    </div>
+  );
+};
+
+const FromularioCreacionVehiculos = ({
+  funcionParaMostrarLaTabla, 
+  listaVehiculos, 
+  funcionParaAgregarUnVehiculo, 
+}) => {
+  const [nombre, setNombre] = useState();
+  const [marca, setMarca] = useState();
+  const [modelo, setModelo] = useState();
+
+  const enviarAlBackend = ()=>{
+    console.log("nombre", nombre, "marca", marca, "modelo", modelo);
+    toast.success("Vehículo creado con éxito");
+    funcionParaMostrarLaTabla(true);
+    funcionParaAgregarUnVehiculo([
+      ...listaVehiculos,
+      {nombre: nombre, marca: marca, modelo: modelo}, 
+    ]);
+  };
 
   return (
-    <form className='flex flex-col'>
-      <h2>Formulario de Creación de vehículos</h2>
-      <label htmlFor='edad'>
-        Por favor ingrese su edad
-        <input 
-          value={edad} 
-          onChange={(e)=>
-            {setEdad(e.target.value);
-          }}
-          className='border border-gray-600' 
-          name='edad' 
-          type="number" 
-        />
-      </label>
-      {esMenorDeEdad ? (
-        <span className='text-3xl text-red-500'>
-          ¡Usted es menor de edad, no puede hacer pagos!
-        </span> 
-        ) : (
-          <span className='text-3xl text-green-500'>
-            ¡Usted es mayor de edad, puede hacer pagos!
-          </span>
-        )}
+    <div className='flex flex-col items-center justify-center'>
+      <h2 className='text-2xl font-extrabold text-gray-800'>Crear nuevo vehículo</h2>
+      <form className='flex flex-col'>
+        <label className='flex flex-col' htmlFor='nombre'>
+          Nombre del vehículo
+          <input 
+            name='nombre'
+            className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' 
+            type="text" 
+            placeholder='Corolla'
+            value={nombre}
+            onChange={(e) => {
+              setNombre(e.target.value);
+            }}
+          />
+        </label>
+        <label className='flex flex-col' htmlFor='marca'>
+          Marca del vehículo
+          <select 
+             value={marca}
+             onChange={(e) => {
+               setMarca(e.target.value);
+             }}           
+             name='marca' 
+             className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' 
+          >
+             <option disabled>Seleccion una opción</option>
+             <option>Renault</option>
+             <option>Toyota</option>
+             <option>Ford</option>
+             <option>Mazda</option>
+             <option>Chevrolet</option>
+          </select>
+        </label>
+        <label className='flex flex-col' htmlFor='modelo'>
+          Modelo del vehículo
+          <input 
+            name='modelo'
+            className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' 
+            type="number" 
+            min={1992}
+            max={2022}
+            placeholder='2014'
+            value={modelo}
+            onChange={(e) => {
+              setModelo(e.target.value);
+            }}
+          />
+        </label>
         <button 
-          onClick={() => setMostrarCamposAdicionales(!mostrarCamposAdicionales)} 
-          type="button" 
-          className='text-white bg-indigo-500'
+          type='button'
+          className='col-span-2 bg-green-400 p-2 rounded-full shadow-md hover:bg-green-600 text-white'
+          onClick={()=>{
+            enviarAlBackend();
+          }}
         >
-          Mostrar campos adicionales
+          Guardar Vehiculo
         </button>
-        {mostrarCamposAdicionales && (
-          <div>
-            <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type="text" />
-            <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type="text" />
-            <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type="text" />
-            <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type="text" />
-            <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type="text" />
-          </div>
-        )}
-    </form>
+      </form>
+    </div>
   );
-};   
+};
+
 
 export default Vehiculos;
